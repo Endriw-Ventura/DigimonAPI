cb63c4de984abcc673e7ee215669ae5197bc3859
Endriw Ventura <endriwventura@yahoo.com>
2026-06-15 21:43:53 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/DigimonController.cs
@@ -0,0 +1,48 @@
+﻿using DigimonAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigimonAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DigimonController : ControllerBase
+    {
+        private readonly IDigimonService _digimonService;
+
+        public DigimonController(IDigimonService digimonService)
+        {
+            _digimonService = digimonService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDigimons()
+        {
+            var digimons = await _digimonService.GetAllDigimons();
+            return Ok(digimons);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetDigimonByName(string name)
+        {
+           var digimon = await _digimonService.GetDigimonByName(name);
+            if (digimon is null)
+                return NotFound();
+
+            return Ok(digimon);
+        }
+
+        [HttpGet("level/{level}")]
+        public async Task<IActionResult> GetDigimonByLevel(string level)
+        {
+            var digimons = await _digimonService.GetDigimonsByLevel(level);
+            return Ok(digimons);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> GetDigimonBySearchBar([FromQuery] string search)
+        {
+            var digimons = await _digimonService.GetDigimonsBySeachBar(search);
+            return Ok(digimons);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using DigimonAPI.Application.Interfaces;
+using DigimonAPI.Application.Services;
+using DigimonAPI.Infrastructure.Integrations.DigimonIntegration;
+using DigimonAPI.Infrastructure.Repositories.DigimonRepository;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddHttpClient<IDigimonIntegration, DigimonIntegration>((client) =>
+{
+    client.BaseAddress = new Uri(builder.Configuration.GetSection("ExternalApis:DigimonApi:baseUrl").Value!);
+});
+builder.Services.AddScoped<IDigimonService, DigimonService>();
+builder.Services.AddScoped<IDigimonRepository, DigimonRepository>();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
